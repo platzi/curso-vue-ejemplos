a3532e93668b2edc8d83a9ff4585e2512273d987
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-07-11 12:34:31 -0300

    Adding example

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template lang="pug">
   #app
     img(src="./assets/logo.png")
-    h1 Ciclo de Vida
+    h1 Slots
+
+    child
+      h1(slot="title") Titulo Slot
+      div(slot="body")
+        p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </template>
 
 <script>
+import Child from './ChildComponent.vue'
+
 export default {
   name: 'app',
 
-  created () {
-    console.log('created...')
-  },
+  components: { Child },
 
-  mounted () {
-    console.log('mounted...')
+  data () {
+    return {
+
+    }
   }
 }
 </script>
---
+++ src/ChildComponent.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  div
+    slot(name="title")
+      h1 Titulo
+    slot(name="body")
+      p Cuerpo
+</template>
